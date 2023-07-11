1350d498670af06fd0bbc3bc745a17ea1d9f7c50
Shukui Zhang <shookui@gmail.com>
2023-07-11 10:03:42 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "os"
+)
+func main() {
+  port := os.Getenv("PORT")
+  if port == "" {
+      port = "8080"
+  }
+  http.HandleFunc("/v1/", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "{status: 'running'}")
+  })
+  log.Println("Pets REST API listening on port", port)
+  if err := http.ListenAndServe(":"+port, nil); err != nil {
+      log.Fatalf("Error launching Pets REST API server: %v", err)
+  }
+}
